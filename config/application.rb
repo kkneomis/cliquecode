8d64fdd95ae456b47be272f85a4824f1e5497919
Simeon Kakpovi <skakpovi@gmail.com>
2015-02-23 01:24:06 +0000

    F

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,6 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Clique
   class Application < Rails::Application
+     config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,6 +24,6 @@ module Clique
     # config.i18n.default_locale = :de
   end
   
-  config.assets.initialize_on_precompile = false
+ 
   
 end
